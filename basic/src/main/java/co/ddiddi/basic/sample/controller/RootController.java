83f5a09dc08f9153669f359ff160a51c27e28f0e
Kim/NamGyu <kimng0222@gmail.com>
2023-01-06 14:37:44 +0900

    빌드 오류 수정

--- basic/src/main/java/co/ddiddi/basic/sample/controller/RootController.java
+++ basic/src/main/java/co/ddiddi/basic/sample/controller/RootController.java
@@ -5,7 +5,6 @@ import java.util.UUID;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 import co.ddiddi.basic.sample.vo.SampleVo;
 
